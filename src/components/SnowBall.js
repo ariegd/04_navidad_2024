005643def3a332df2d46a0a1c1856e17de1088a2
zodd <zodd@nootbster.home>
2024-12-22 23:42:14 +0100

    SnowBall 1

--- src/components/SnowBall.js
+++ src/components/SnowBall.js
@@ -7,8 +7,18 @@ class SnowBall extends HTMLElement {
     static get styles() {
         return /* css */`
             :host {
-            
             }
+
+            .container {
+                --size: 500px;
+
+                border: 1px solid #fff;
+                width: var(--size);
+                height: var(--size);
+                border-radius: 50%;
+            }
+            
+        
         `;
     }
 
@@ -19,8 +29,17 @@ class SnowBall extends HTMLElement {
     render() {
         this.shadowRoot.innerHTML = /* html */`
         <style>${SnowBall.styles}</style>
-        <div>
+        <div class="container">
         </div>
+        <h2>HTML Forms</h2>
+
+        <form action="/action_page.php">
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname" value="John"><br>
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname" value="Doe"><br><br>
+            <input type="submit" value="Submit">
+        </form> 
         `;
     }
 }
